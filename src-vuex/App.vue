6dcbcb5a1b12d43985afebacb82b4b4600a1fb04
小飞 <1967447613@qq.com>
2019-04-16 17:34:12 +0800

    first commit

---
+++ src-vuex/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <button @click="add">点击</button>
+        <p>{{num}}</p>
+    </div>
+</template>
+<script>
+import { mapMutations } from 'vuex'
+export default {
+    name:'app',
+    methods:{
+        ...mapMutations(['add']),
+    },
+    computed:{
+    num(){
+      let {count} = this.$store.state;
+      return count < 10?'0'+ count:''+count
+    }
+  },
+}
+</script>
